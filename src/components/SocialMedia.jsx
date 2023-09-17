c470e3031fbdc0f61975687f1b0b9b19e8237146
SAIKmar-1729 <83260137+SAIKmar-1729@users.noreply.github.com>
2023-09-17 23:39:27 +0530

    Final commit phase-1

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {BsTwitter,BsInstagram} from 'react-icons/bs';
-import {FaFacebookF} from 'react-icons/fa';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
     return (
         <div className='app__social'>
             <div>
-                <BsTwitter/>
+                <BsTwitter />
             </div>
             <div>
-                <BsInstagram/>
+                <a href='https://www.instagram.com/saikumar._.1729/' target="_blank" rel="noopener noreferrer"><BsInstagram /></a>
             </div>
             <div>
-                <FaFacebookF/>
+                <FaFacebookF />
             </div>
         </div>
     )
